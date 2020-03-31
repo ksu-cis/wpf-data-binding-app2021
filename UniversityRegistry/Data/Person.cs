e5f56eb3e3c655f40e029d1ab17e81eb6e2e0135
Nick <nickolas.appino@gmail.com>
2020-03-31 18:30:18 -0500

    backup

--- UniversityRegistry/Data/Person.cs
+++ UniversityRegistry/Data/Person.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace UniversityRegistry.Data
 {
     /// <summary>
     /// A class representing a person associated with the university
     /// </summary>
-    public class Person
+    public class Person: INotifyPropertyChanged
     {
         /// <summary>
         /// The next ID to assign to a newly-created person
         /// </summary>
         public static uint NextID = 80000000;
+        
+        /// <summary>
+        /// Event triggered when properties of Person change 
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
 
         /// <summary>
         /// The uinque identifier of this person
@@ -22,28 +28,121 @@ namespace UniversityRegistry.Data
         /// <summary>
         /// The person's first name
         /// </summary>
-        public string FirstName { get; set; }
+        private string firstName;
+        /// <summary>
+        /// The person's first name
+        /// </summary>
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (firstName == value) return;
+                firstName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("FirstName"));
+            }
+        }
 
         /// <summary>
         /// The person's last name
         /// </summary>
-        public string LastName { get; set; }
+        private string lastName;
+        /// <summary>
+        /// The person's last name
+        /// </summary>
+        public string LastName
+        {
+            get { return lastName; }
+            set
+            {
+                if (lastName == value) return;
+                lastName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("LastName"));
+            }
+        }
 
         /// <summary>
         /// The person's date of birth
         /// </summary>
-        public DateTime DateOfBirth { get; set; }
+        private DateTime dateOfBirth;
+        /// <summary>
+        /// The person's date of birth
+        /// </summary>
+        public DateTime DateOfBirth
+        {
+            get { return dateOfBirth; }
+            set
+            {
+                if (dateOfBirth == value) return;
+                dateOfBirth = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("DateOfBirth"));
+            }
+        }
 
+        /// <summary>
+        /// If this person is active in the university
+        /// </summary>
+        private bool active;
         /// <summary>
         /// If this person is active in the university (currently a part of the university)
         /// </summary>
-        public bool Active { get; set; }
+        public bool Active
+        {
+            get { return active; }
+            set
+            {
+                if (active == value) return;
+                active = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Active"));
+            }
+        }
 
+        public bool IsFaculty
+        {
+            get { return Role == Role.Faculty; }
+            set { Role = Role.Faculty; }
+        }
+
+        public bool IsUndergraduateStudent
+        {
+            get { return Role == Role.UndergraduateStudent; }
+            set { Role = Role.UndergraduateStudent; }
+        }
+
+        public bool IsGraduateStudent
+        {
+            get { return Role == Role.GraduateStudent; }
+            set { Role = Role.GraduateStudent; }
+        }
+
+        public bool IsStaff
+        {
+            get { return Role == Role.Staff; }
+            set { Role = Role.Staff; }
+        }
+
+
+        private Role role;
         /// <summary>
         /// The person's role
         /// </summary>
-        public Role Role { get; set; }
-        
+        public Role Role
+        {
+            get => role;
+            set
+            {
+                if (role == value) return;
+                role = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Role"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsUndergraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsGraduateStudent"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsFaculty"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("IsStaff"));
+            }
+        }
+
+
         /// <summary>
         /// Creates a new user, assigning them an ID
         /// </summary>
@@ -51,5 +150,16 @@ namespace UniversityRegistry.Data
         {
             ID = NextID++;
         }
+
+        /// <summary>
+        /// Returns a string identifying the person
+        /// </summary>
+        /// <returns>A string consisting of last name, first name, and ID</returns>
+        public override string ToString()
+        {
+            return $"{LastName}, {FirstName} [{ID}]";
+        }
+
+
     }
 }
